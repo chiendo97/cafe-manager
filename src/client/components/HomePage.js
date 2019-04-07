11aa5a230ab28bba2251c9df547d3e6b2cfe193c
Le Tien Chien <chiendo97@Les-MacBook-Pro.net.fpt>
2019-04-07 20:58:28 +0700

    Mini table at home page

--- src/client/components/HomePage.js
+++ src/client/components/HomePage.js
@@ -1,16 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Container, Row, Col } from 'react-bootstrap';
+
+import { MiniMenu } from './MiniMenu'
+import { MiniStorage } from './MiniStorage'
+import { MiniReceipt } from './MiniReceipt'
 
 class HomePage extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>
-					Home Page Here!!
-				</h1>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h1>
+          Cafe manager
+        </h1>
+        <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
+          <Row style={{ marginLeft: 0, marginRight: 0}}>
+            <Col style={{ paddingLeft: 0, paddingRight: 0}}> <MiniMenu></MiniMenu> </Col>
+            <Col style={{ paddingLeft: 0, paddingRight: 0}}> <MiniStorage></MiniStorage> </Col>
+          </Row>
+          <Row style={{ marginLeft: 0, marginRight: 0}}>
+            <Col style={{ paddingLeft: 0, paddingRight: 0}}> <MiniReceipt></MiniReceipt> </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export { HomePage }
