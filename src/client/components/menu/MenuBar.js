75c9a98e4e250b873719532a7d56b56fbb5149f0
chiendo97 <chiendo97@gmail.com>
2019-05-11 19:02:43 +0700

    Fix menubar show menu with employee account

--- src/client/components/menu/MenuBar.js
+++ src/client/components/menu/MenuBar.js
@@ -1,38 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import { Link } from 'react-router-dom'
 import { Container, Menu } from 'semantic-ui-react'
 
 class MenuBar extends Component {
-
   render() {
-
     const { user } = this.props
 
     return (
       <div>
-        <Menu color='black' fixed='top' inverted>
+        <Menu color="black" fixed="top" inverted>
           <Container>
-            <Menu.Item children={<Link to='/'>Cafe-Manager</Link>} />
-            {user && user.role === 'admin' &&
-              <Menu.Item children={<Link to='/users'>Users</Link>} />}
-            {user &&
-              <Menu.Item children={<Link to='/menu'>Menu</Link>}></Menu.Item>}
-            {user &&
-              <Menu.Item children={<Link to='/receipt'>Receipt</Link>}></Menu.Item>}
-            {user && (user.role === 'manager' || user.role === 'admin') &&
-              <Menu.Item children={<Link to='/storage'>Storage</Link>}></Menu.Item>}
-            {
-              user ?
-                <Menu.Item as='a' position="right" href="/login">Hello {user.firstname}, Logout</Menu.Item>
-                :
-                <Menu.Item as='a' position="right" href="/login">Login</Menu.Item>
-            }
+            <Menu.Item children={<Link to="/">Cafe-Manager</Link>} />
+            {user && user.role === 'admin' && (
+              <Menu.Item children={<Link to="/users">Users</Link>} />
+            )}
+            {user && (user.role === 'manager' || user.role === 'admin') && (
+              <Menu.Item children={<Link to="/menu">Menu</Link>} />
+            )}
+            {user && (
+              <Menu.Item children={<Link to="/receipt">Receipt</Link>} />
+            )}
+            {user && (user.role === 'manager' || user.role === 'admin') && (
+              <Menu.Item children={<Link to="/storage">Storage</Link>} />
+            )}
+            {user ? (
+              <Menu.Item as="a" position="right" href="/login">
+                Hello {user.firstname}, Logout
+              </Menu.Item>
+            ) : (
+              <Menu.Item as="a" position="right" href="/login">
+                Login
+              </Menu.Item>
+            )}
           </Container>
         </Menu>
       </div>
-    );
+    )
   }
 }
 
-export default MenuBar;
+export default MenuBar
